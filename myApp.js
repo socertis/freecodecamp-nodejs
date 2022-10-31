313ef9952fc6db56cdbe79b7e0ebb600eeb96af7
basem <basem.taha@optomatica.com>
2022-10-31 19:04:22 +0200

    ex5

--- myApp.js
+++ myApp.js
@@ -3,14 +3,20 @@ let app = express();
 
 console.log('Hello World');
 
+app.use('/', express.static(__dirname+'/public'));
+
 app.get('/',
     (req, res) => {
         res.sendFile(__dirname+'/views/'+'index.html');
     }
 );
 
+app.get('/json', (req, res) => {
+    res.json({
+        message: "Hello json"
+    });
+});
 
-app.use('/public', express.static(__dirname+'/public'));
 
 
 
