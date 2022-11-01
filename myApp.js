5714ae7a8e22719ecb654d54ee97a3c1f5b4f903
Basem Ezzat <2311445+socertis@users.noreply.github.com>
2022-11-01 09:49:32 +0000

    ex10

--- myApp.js
+++ myApp.js
@@ -36,5 +36,16 @@ app.get('/now', (req, res, next) =>{
   });
 });
 
+app.get('/:word/echo', (req, res) =>{
+  res.json({
+    echo: req.params.word
+  });
+});
+
+app.route('/name').get((req, res)=>{
+  res.json({
+    name: req.query.first + " " + req.query.last
+  });
+});
 
  module.exports = app;
