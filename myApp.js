4703af7ca08aaf0a3ed9ad069645d0809d48124f
Basem Ezzat <2311445+socertis@users.noreply.github.com>
2022-11-01 09:28:52 +0000

    ex8

--- myApp.js
+++ myApp.js
@@ -27,33 +27,14 @@ app.get('/json', (req, res) => {
     });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/now', (req, res, next) =>{
+  req.time = new Date().toString();
+  next();
+} ,(req, res) => {
+  res.json({
+    time: req.time
+  });
+});
 
 
  module.exports = app;
